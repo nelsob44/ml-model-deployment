39ddbd64c0539073588ab2c62921c0f1c5bcedee
nelsob44 <nelsob44@gmail.com>
2024-09-13 16:27:41 +0100

    Added config file

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+import os
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
 import pickle
+load_dotenv()
 
 tokenizer = pickle.load(open("models/cv.pkl", "rb"))
 model = pickle.load(open("models/clf.pkl", "rb"))
 
 app = Flask(__name__)
+
+env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 @app.route("/")
 
@@ -20,4 +26,4 @@ def predict():
     prediction = 1 if predictions == 1 else -1
     return render_template('index.html', prediction=prediction, email_text=email_text)
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000)
