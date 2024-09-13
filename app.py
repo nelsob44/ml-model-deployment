aa7758faf0866915b9750563c6cd86f140488f70
nelsob44 <nelsob44@gmail.com>
2024-09-13 15:29:09 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pickle
+
+tokenizer = pickle.load(open("models/cv.pkl", "rb"))
+model = pickle.load(open("models/clf.pkl", "rb"))
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+    
+    return render_template('index.html')
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email_text = request.form.get('email-content')
+    tokenized_email = tokenizer.transform([email_text])
+    predictions = model.predict(tokenized_email)
+    prediction = 1 if predictions == 1 else -1
+    return render_template('index.html', prediction=prediction, email_text=email_text)
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
